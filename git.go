64f4d12ac88bf6125e13f1acc6750bd9c1d619c5
Muyiwa Jimi-Salami <muyiwa@shyp.com>
2016-02-02 09:59:39 -0800

    Use Last Commit Message
    When creating a pull request

--- git.go
+++ git.go
@@ -134,11 +134,23 @@ func CurrentBranch() (string, error) {
 	return strings.TrimSpace(string(result)), nil
 }
 
+func getLastCommitMessage() (string, error) {
+	result, err := exec.Command("git", "log", "-1", "--pretty=%B").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(result)), nil
+}
+
 // CreateAndOpenPullRequest creates a PR on Github and opens it up in your browser
 // Returns an error if you are not on a branch, or if you are not in a git repository.
-func CreateAndOpenPullRequest(title string) error {
+func CreateAndOpenPullRequest() error {
+	message, err := getLastCommitMessage()
+	if err != nil {
+		return err
+	}
 	cmd := commands.CmdRunner.Lookup("pull-request")
-	args := commands.NewArgs([]string{"pull-request", "-m", title, "-o"})
+	args := commands.NewArgs([]string{"pull-request", "-m", message, "-o"})
 	execError := commands.CmdRunner.Call(cmd, args)
 	return execError.Err
 }
